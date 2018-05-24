d539844e410a28321f0b713f0af32afba355b34d
James Tuesday Paschall <jamestpaschall@Big-Brain.local>
2018-05-23 17:58:06 -0700

    added the missing html files

--- apps/campfriends/urls.py
+++ apps/campfriends/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
 	url(r'^$', views.info),
-	url(r'^info$', views.info, name='info'),
-	url(r'^add_info$', views.add_info, name='add_info'),
-	url(r'^process_info$', views.process_info, name='process_info'),
+	url(r'^campfriends/info$', views.info, name='info'),
+	url(r'^campfriends/add_info$', views.add_info, name='add_info'),
+	url(r'^campfriends/process_info$', views.process_info, name='process_info'),
 ]
