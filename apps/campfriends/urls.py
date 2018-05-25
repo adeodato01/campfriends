d28adc31e7fe0a78fe5d1340601fab92b3f731ef
adeodato01 <arvin_deodato@hotmail.com>
2018-05-24 18:06:07 -0700

    got the coordinates to be submitted via GET

--- apps/campfriends/urls.py
+++ apps/campfriends/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
 	url(r'^info$', views.info, name='info'),
-	url(r'^add_info$', views.add_info, name='add_info'),
+	url(r'^add_info/(?P<location>\d+)', views.add_info, name='add_info'),
 	url(r'^process_info$', views.process_info, name='process_info'),
 	url(r'^logout$', views.logout, name='logout'),
 	url(r'^travels$', views.travels, name='travels'),
